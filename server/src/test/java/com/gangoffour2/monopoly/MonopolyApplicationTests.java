55b2d96046484754cd60e739e00b142991d20ff4
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-05 18:05:11 +0100

    First commit

---
+++ server/src/test/java/com/gangoffour2/monopoly/MonopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangoffour2.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
