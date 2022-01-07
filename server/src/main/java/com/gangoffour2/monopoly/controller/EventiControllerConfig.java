aa02ed1aaba90bcef279832ba668d78b03664fae
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-07 13:49:02 +0100

    Aggiunta configurazione per le WebSocket

---
+++ server/src/main/java/com/gangoffour2/monopoly/controller/EventiControllerConfig.java
@@ -0,0 +1,18 @@
+package com.gangoffour2.monopoly.controller;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class EventiControllerConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new EventiController(), "/partite/{id}");
+    }
+}
