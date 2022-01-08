d0898ed6a33e3cba9b521a38d66337123ccd4d53
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-08 14:45:24 +0100

    Aggiunto test per il cambio degli stati del terreno basato sulle azioni dei giocatori

--- server/src/test/java/com/gangoffour2/monopoly/MonopolyApplicationTests.java
+++ server/src/test/java/com/gangoffour2/monopoly/MonopolyApplicationTests.java
@@ -1,13 +1,33 @@
 package com.gangoffour2.monopoly;
 
+import com.gangoffour2.monopoly.azioni.giocatore.*;
+import com.gangoffour2.monopoly.model.casella.*;
+import com.gangoffour2.monopoly.stati.casella.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class MonopolyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void cambioStatiTerreno(){
+
+		Terreno t = Terreno.builder().build();
+
+		t.setState(TerrenoNonAcquistato.builder().build());
+		t.getStatoCorrente().setTerreno(t);
+
+		t.onAzioneGiocatore(AcquistaProprieta.builder().build());
+		assertTrue(t.getStatoCorrente() instanceof TerrenoAcquistato);
+
+		t.onAzioneGiocatore(Ipoteca.builder().build());
+		assertTrue(t.getStatoCorrente() instanceof TerrenoIpotecato);
 	}
 
 }
