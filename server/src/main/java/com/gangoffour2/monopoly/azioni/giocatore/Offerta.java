1a2fa1cd05457f10d357b2565168ba91bebc57c7
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-11 23:23:24 +0100

    Aggiunta azione giocatore

---
+++ server/src/main/java/com/gangoffour2/monopoly/azioni/giocatore/Offerta.java
@@ -0,0 +1,16 @@
+package com.gangoffour2.monopoly.azioni.giocatore;
+
+import com.gangoffour2.monopoly.model.casella.Casella;
+import com.gangoffour2.monopoly.stati.partita.StatoPartita;
+
+public class Offerta extends AzioneGiocatore {
+    @Override
+    public void accept(Casella casella) {
+
+    }
+
+    @Override
+    public void accept(StatoPartita statoPartita) throws InterruptedException {
+
+    }
+}
