6e92db2d3f3f1002212b33bca2874f48c65f1e50
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-14 00:25:24 +0100

    Aggiornato metodo per entrare nella partita

--- server/src/main/java/com/gangoffour2/monopoly/controller/EventiController.java
+++ server/src/main/java/com/gangoffour2/monopoly/controller/EventiController.java
@@ -3,22 +3,43 @@ package com.gangoffour2.monopoly.controller;
 import com.gangoffour2.monopoly.azioni.giocatore.AcquistaProprieta;
 import com.gangoffour2.monopoly.azioni.giocatore.Ipoteca;
 import com.gangoffour2.monopoly.azioni.giocatore.Offerta;
+import com.gangoffour2.monopoly.eccezioni.GiocatoreEsistenteException;
+import com.gangoffour2.monopoly.eccezioni.PartitaPienaException;
+import com.gangoffour2.monopoly.model.Partita;
+import com.gangoffour2.monopoly.services.FactoryPartita;
+import com.gangoffour2.monopoly.services.PartiteRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class EventiController {
 
-
-
     @MessageMapping("/partite/{id}/entra")
-    public void entraInPartita(@Payload String message) {
-
+    public void entraInPartita(@Payload String nick, @DestinationVariable String id) {
+        Partita partita = PartiteRespository.getInstance().getPartitaByid(id);
+        if(partita != null){
+            try{
+                partita.aggiungiGiocatore(FactoryPartita.getInstance().playerFromNickname(nick, partita));
+            }catch(PartitaPienaException e){
+                Map<String, Object> headers = new HashMap<>();
+                headers.put("nickname", nick);
+                MessageBrokerSingleton.getInstance().getTemplate()
+                        .convertAndSend("/topic/partite/" + id, "Partita piena", headers);
+            }catch(GiocatoreEsistenteException e){
+                Map<String, Object> headers = new HashMap<>();
+                headers.put("nickname", nick);
+                MessageBrokerSingleton.getInstance().getTemplate()
+                    .convertAndSend("/topic/partite/" + id,"Nome già esistente", headers);
+            }
+        }
     }
 
     @MessageMapping("/partite/{id}/esci")
