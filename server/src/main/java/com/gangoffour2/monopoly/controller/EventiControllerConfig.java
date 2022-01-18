ee821d97ce67ffa3ffb64b585530e1bd8c9ad346
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-18 23:14:01 +0100

    aggiunto il cors al server

--- server/src/main/java/com/gangoffour2/monopoly/controller/EventiControllerConfig.java
+++ server/src/main/java/com/gangoffour2/monopoly/controller/EventiControllerConfig.java
@@ -13,7 +13,7 @@ public class EventiControllerConfig implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp");
+        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*");
     }
 
     @Override
--- server/src/main/java/com/gangoffour2/monopoly/controller/WebController.java
+++ server/src/main/java/com/gangoffour2/monopoly/controller/WebController.java
@@ -4,15 +4,14 @@ import com.gangoffour2.monopoly.model.Configurazione;
 import com.gangoffour2.monopoly.model.Partita;
 import com.gangoffour2.monopoly.services.FactoryPartita;
 import com.gangoffour2.monopoly.services.PartiteRespository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class WebController {
+
+    @CrossOrigin
     @PostMapping(value = "/partite")
     public String creaPartita(@RequestBody Configurazione configurazione) {
         Partita nuovaPartita = FactoryPartita.getInstance().creaPartita(configurazione);
