d582ea03eec9483e13a3a9b92c443394b760e141
PhantoNull <l.dipierro@campus.unimib.it>
2022-01-22 16:21:10 +0100

    Aggiunta funzione muoviGiocatoreProssimoTipoCasella per imprevisti e probabilità Modificata Carta e CartaModificaDenaro

--- server/src/main/java/com/gangoffour2/monopoly/model/carta/Carta.java
+++ server/src/main/java/com/gangoffour2/monopoly/model/carta/Carta.java
@@ -1,5 +1,6 @@
 package com.gangoffour2.monopoly.model.carta;
 
+import com.gangoffour2.monopoly.model.Giocatore;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
@@ -9,6 +10,6 @@ import java.io.Serializable;
 @SuperBuilder
 public abstract class Carta implements Serializable {
     private String testo;
-    public abstract void esegui();
+    public abstract void esegui(Giocatore g);
     public abstract void effetto();
 }
