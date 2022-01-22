f325b9a67b2f8a924ee21e3021bb6aa46864c75f
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-22 12:12:37 +0100

    Separata dalla partita la gestione dei timeout

---
+++ server/src/main/java/com/gangoffour2/monopoly/services/TimeoutHandler.java
@@ -0,0 +1,24 @@
+package com.gangoffour2.monopoly.services;
+
+public class TimeoutHandler {
+    private Thread handlerThread;
+
+
+    public void setTimeout(Runnable runnable, int millisecondi){
+        handlerThread = new Thread(() -> {
+            try {
+                Thread.sleep(millisecondi);
+                runnable.run();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+        });
+        handlerThread.start();
+    }
+
+
+    public void stopTimeout(){
+        handlerThread.interrupt();
+    }
+}
