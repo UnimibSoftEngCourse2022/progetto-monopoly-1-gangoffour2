a3157324e537ae74c9ce40eb6eee02324d3792ea
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-23 19:49:18 +0100

    aggiunte le varie casella nella traduzione del polimorfismo

--- client/src/component/caselle/Casella.tsx
+++ client/src/component/caselle/Casella.tsx
@@ -2,10 +2,22 @@ import ICasella from "../../interfaces/caselle/ICasella";
 import React from "react";
 import {Imprevisto} from "./Imprevisto";
 import {Terreno} from "./Terreno";
+import {Societa} from "./Societa";
+import {Stazione} from "./Stazione";
+import Prigione from "./Prigione";
+import {Probabilita} from "./Probabilita";
+import {Tassa} from "./Tassa";
+import {VaiInPrigione} from "./VaiInPrigione";
 
 const translate = {
     "Imprevisto": (props: any) => <Imprevisto casella={props}/>,
-    "Terreno": (props: any) => <Terreno casella={props}/>
+    "Terreno": (props: any) => <Terreno casella={props}/>,
+    "Societa": (props: any) => <Societa casella={props}/>,
+    "Stazione": (props: any) => <Stazione casella={props}/>,
+    "Prigione": (props: any) => <Prigione casella={props}/>,
+    "Probabilita": (props: any) => <Probabilita casella={props}/>,
+    "Tassa": (props: any) => <Tassa casella={props}/>,
+    "VaiInPrigione": (props: any) => <VaiInPrigione casella={props}/>
 }
 
 export class Casella extends React.Component<ICasella, {}>{
