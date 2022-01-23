5548a6df80bb2d5615a967220a22215a1abe5a69
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-23 10:11:09 +0100

    aggiunto react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Home from "./pages/home/Home";
 
 function App() {
+
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
