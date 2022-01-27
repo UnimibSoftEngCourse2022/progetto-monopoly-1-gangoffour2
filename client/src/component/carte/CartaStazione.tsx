5b42f8efe5997c15af683c6752e1789516b248f6
Gabriele Canesi <g.canesi1@campus.unimib.it>
2022-01-27 18:10:50 +0100

    Aggiunte carte per le proprietà

---
+++ client/src/component/carte/CartaStazione.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './carta.css';
+import ICasellaStazione from "../../interfaces/caselle/ICasellaStazione";
+
+interface Props{
+    casella: ICasellaStazione;
+}
+
+export default class CartaStazione extends React.Component<Props, {}>{
+
+
+    render() : JSX.Element {
+
+        let {casella} = this.props;
+        return <div className="table-carta">
+            <div className="carta-d">
+                <summary className="terreno-carta grigio">
+                    <span className="eyebrow"/>
+                    {casella.nome}
+                </summary>
+                <p className="hotel">Rendita: {}</p>
+                <p className="disclaimer">If a player owns ALL the Lots of any Color-Group, the<br/>rent is Doubled on
+                    Unimproved Lots in that group.<br/>&copy;1935 Hasbro, Inc.</p>
+            </div>
+        </div>
+    }
+}
--- client/src/pages/partita/popup/PopupAcquisto.tsx
+++ client/src/pages/partita/popup/PopupAcquisto.tsx
@@ -3,7 +3,20 @@ import Popup from "../../../component/popup/Popup";
 import StompController from "../../../application/stompController";
 import PopupButtonContainer from "../../../component/popup/PopupButtonContainer";
 import {PopupProps} from "./PopupRouter";
+import CartaTerreno from "../../../component/carte/CartaTerreno";
+import ICasellaTerreno from "../../../interfaces/caselle/ICasellaTererno";
+import ICasella from "../../../interfaces/caselle/ICasella";
+import ICasellaSocieta from "../../../interfaces/caselle/ICasellaSocieta";
+import CartaSocieta from "../../../component/carte/CartaSocieta";
+import CartaStazione from "../../../component/carte/CartaStazione";
+import ICasellaStazione from "../../../interfaces/caselle/ICasellaStazione";
 
+
+const translate_carta : any = {
+    "Terreno": (terreno: ICasellaTerreno) => <CartaTerreno casella={terreno}/>,
+    "Societa": (societa: ICasellaSocieta) => <CartaSocieta casella={societa}/>,
+    "Stazione": (stazione: ICasellaStazione) => <CartaStazione casella={stazione}/>
+}
 
 interface State {
 
@@ -14,23 +27,24 @@ export default class PopupAcquisto extends React.Component<PopupProps, State> {
     render() {
 
         const nome = this.props.giocatore.casellaCorrente.nome;
-
+        let casellaCorrente: ICasella = this.props.giocatore.casellaCorrente;
         let testo = "";
-        if(this.props.isMioTurno)
-            testo = "Sei finito sulla proprietà " + nome + " comprala o mettila all'asta";
-        else
+        if(!this.props.isMioTurno)
             testo = "Il giocatore " + this.props.giocatore.nick + " è finito sulla proprietà: " + nome + " attendi che la compri o la metta all'asta"
 
         return (
             <div>
                 <Popup trigger={true} onClose={() => {}} title={"Acquista la proprietà"}>
-                    <p>{testo}</p>
-                    {this.props.isMioTurno &&
-                        <PopupButtonContainer>
-                            <button onClick={() => StompController.acquista()}>Compra</button>
-                            <button>Metti all'asta</button>
-                        </PopupButtonContainer>
-                    }
+                    <div className={"container-popup-acquisto"}>
+                        <p>{testo}</p>
+                        {this.props.isMioTurno &&
+                            <PopupButtonContainer>
+                                <button onClick={() => StompController.acquista()}>Compra</button>
+                                <button onClick = {() => StompController.avviaAsta()}>Metti all'asta</button>
+                            </PopupButtonContainer>
+                        }
+                        {translate_carta[casellaCorrente.type](casellaCorrente)}
+                    </div>
                 </Popup>
             </div>
         )
