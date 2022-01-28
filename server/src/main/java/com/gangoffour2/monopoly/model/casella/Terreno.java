fa8342d1d18b269c83c3eafd641c5372f4552ced
PhantoNull <l.dipierro@campus.unimib.it>
2022-01-28 18:13:53 +0100

    Fix terreno

--- server/src/main/java/com/gangoffour2/monopoly/model/casella/Terreno.java
+++ server/src/main/java/com/gangoffour2/monopoly/model/casella/Terreno.java
@@ -29,33 +29,32 @@ public class Terreno extends Proprieta {
     }
     @Override
     public void randomizzaCasella(float m){
-        setCostoBase((int) Math.floor(getCostoBase()*m));
-        setIpoteca((int) Math.floor(getIpoteca()*m));
-        setRendita((int) Math.floor(getRendita()*m));
-        for(int i = 0; i < affitti.size(); i++)
-            getAffitti().set(i, (int) Math.floor(getAffitti().get(i)*m));
+        costoBase = (int) Math.floor(costoBase*m);
+        ipoteca = (int) Math.floor(ipoteca*m);
+        rendita = (int) Math.floor(rendita*m);
+        for(int i = 0; i < getAffitti().size(); i++)
+            affitti.set(i, (int) Math.floor(affitti.get(i)*m));
 
     }
 
-
     @Override
     public int calcolaAffitto() {
-        if (albergo != null) {
-            return affitti.get(affitti.size() - 1);
-        } else if (listaCase.size() > affitti.size()) {
-            return affitti.get(affitti.size() - 2);
+        if (getAlbergo() != null) {
+            return getAffitti().get(getAffitti().size() - 1);
+        } else if (getListaCase().size() > getAffitti().size()) {
+            return getAffitti().get(getAffitti().size() - 2);
         }
 
         if (!listaCase.isEmpty()) {
-            return affitti.get(listaCase.size() - 1);
+            return getAffitti().get(listaCase.size() - 1);
         }
 
         return rendita;
     }
 
     public void aggiungiEdificio() {
-        if (listaCase.size() == maxCase) {
-            listaCase.add(Casa.builder().build());
+        if (getListaCase().size() == maxCase) {
+            getListaCase().add(Casa.builder().build());
         } else {
             albergo = Albergo.builder().build();
         }
