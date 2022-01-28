28d66aa6f4dd7431d2d9a30d2a7848691b91fd55
PhantoNull <l.dipierro@campus.unimib.it>
2022-01-28 18:19:41 +0100

    Modificato moltiplicatore randomizzaCasella da 0.5 a 2.0]

--- server/src/main/java/com/gangoffour2/monopoly/model/StrategiaRandomEconomiaCasella.java
+++ server/src/main/java/com/gangoffour2/monopoly/model/StrategiaRandomEconomiaCasella.java
@@ -8,7 +8,7 @@ public class StrategiaRandomEconomiaCasella implements RandomCaselleStrategy{
 
     @Override
     public void randomizzaCasella(Casella c) {
-        float moltiplicatore = (float) (new SecureRandom().nextFloat() + 0.5);
+        float moltiplicatore = (float) (new SecureRandom().nextFloat()*1.5 + 0.5);
         c.randomizzaCasella(moltiplicatore);
     }
 }
