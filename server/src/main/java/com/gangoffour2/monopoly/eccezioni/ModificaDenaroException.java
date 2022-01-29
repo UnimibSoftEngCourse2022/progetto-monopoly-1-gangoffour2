f9a1ae80ee72cbed3604a44026ad8bc03a815548
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-29 17:09:57 +0100

    removed unused import

--- server/src/main/java/com/gangoffour2/monopoly/eccezioni/ModificaDenaroException.java
+++ server/src/main/java/com/gangoffour2/monopoly/eccezioni/ModificaDenaroException.java
@@ -1,15 +1,10 @@
 package com.gangoffour2.monopoly.eccezioni;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ModificaDenaroException extends RuntimeException {
     int soldiDaPagare;
-
-    public ModificaDenaroException(){
-        soldiDaPagare = 0;
-    }
 
     public ModificaDenaroException(int soldiDaPagare){
         this.soldiDaPagare = soldiDaPagare;
