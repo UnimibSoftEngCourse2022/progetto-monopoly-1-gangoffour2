cc7a26680d5044d943ee450c7ad1b42713b3e372
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-30 10:14:02 +0100

    risolto warning key client

--- src/main/app/src/component/Tabellone.tsx
+++ src/main/app/src/component/Tabellone.tsx
@@ -23,7 +23,7 @@ export default class Tabellone extends React.Component<Props, State> {
     costruisciRiga(inizio: number, fine: number, classe: string, reverse: boolean){
         let riga : JSX.Element[] = []
         this.props.caselle.slice(inizio, fine).forEach((casella: ICasella) => {
-            riga.push(<Casella {...casella}/>);
+            riga.push(<Casella key={casella.id} {...casella}/>);
         });
         if (reverse){
             riga = riga.reverse();
@@ -36,13 +36,13 @@ export default class Tabellone extends React.Component<Props, State> {
     render() : JSX.Element {
 
         let jsxCaselle = []
-        jsxCaselle.push(<Casella {...this.props.caselle[0]}/>);
+        jsxCaselle.push(<Casella key={this.props.caselle[0].id} {...this.props.caselle[0]}/>);
         jsxCaselle.push(this.costruisciRiga(1, 10, "row horizontal-row bottom-row", true));
-        jsxCaselle.push(<Casella {...this.props.caselle[10]}/>);
+        jsxCaselle.push(<Casella key={this.props.caselle[10].id} {...this.props.caselle[10]}/>);
         jsxCaselle.push(this.costruisciRiga(11, 20, "row vertical-row left-row", true));
-        jsxCaselle.push(<Casella {...this.props.caselle[20]}/>);
+        jsxCaselle.push(<Casella key={this.props.caselle[20].id} {...this.props.caselle[20]}/>);
         jsxCaselle.push(this.costruisciRiga(21, 30, "row horizontal-row top-row", false));
-        jsxCaselle.push(<Casella {...this.props.caselle[30]}/>);
+        jsxCaselle.push(<Casella key={this.props.caselle[30].id} {...this.props.caselle[30]}/>);
         jsxCaselle.push(this.costruisciRiga(31, 40, "row vertical-row right-row", false));
 
         return <div className={"container_tabellone"}>
