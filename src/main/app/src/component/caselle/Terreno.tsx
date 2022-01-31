eff32185984a640f3771c349b30f08a91e3011d8
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-31 13:28:39 +0100

    implementate le case sulle caselle

--- src/main/app/src/component/caselle/Terreno.tsx
+++ src/main/app/src/component/caselle/Terreno.tsx
@@ -12,10 +12,22 @@ interface Props {
 }
 
 export class Terreno extends React.Component<Props, State> {
+
     render() {
+
+        const caseJsx: JSX.Element[] = [];
+
+        for(let i = 0; i < this.props.casella.numeroCase; ++i){
+            caseJsx.push(<div className={"casa"}></div>)
+        }
+
         return <div className="space property">
             <div className = "container" aria-disabled={this.props.casella.stato.type === "TerrenoIpotecato"}>
-                <div className={"color-bar " + this.props.casella.colore}/>
+                <div className={"color-bar " + this.props.casella.colore}>
+                    {
+                        this.props.casella.hasAlbergo ? <div className={"albergo"}></div> : caseJsx
+                    }
+                </div>
                 <div className="name three-line-name">{this.props.casella.nome}</div>
                 <div className="price">{this.props.casella.costoBase}</div>
                 {this.props.children}
